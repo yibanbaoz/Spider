13a7f448fe4bff115dbef90460a6802f2744a260
yibanbaoz <952501069@qq.com>
2018-01-05 20:13:35 +0800

    练习1

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 html_doc = """
 <html><head><title>The Dormouse's story</title></head>
 <body>
-<p class="title"><b>The Dormouse's story</b></p>
+<p class="title attrs"><b>The Dormouse's story</b></p>
 
 <p class="story">Once upon a time there were three little sisters; and their names were
 <a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
@@ -23,4 +23,5 @@ print(type(soup.p['class']))
 for i in soup.find_all('a'):
     print(i.get('href'))
 print('title name is '+soup.title.name)
+print(soup.p['class'])
 
